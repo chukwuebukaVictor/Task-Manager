bae1123492700a8460ed9949c2c6489cb3465a8f
chukwuebukaVictor <chukwuebuka.victor.ozoede@gmail.com>
2022-07-02 06:02:57 +0100

    Setup validation and schema

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -1,9 +1,18 @@
+const Task = require('../models/Task')
+
 const getAllTasks = (req,res)=>{
     res.send('Get all tasks')
   }
-const createTask = (req,res)=>{
+const createTask = async (req,res)=>{
+  try {
+    const task = await Task.create(req.body)
+    res.status(201).json({task})
+    
+  } catch (error) {
+    res.status(500).json({msg:error})  
+  }
     // res.send('Create tasks')
-    res.json(req.body)
+    // res.json(req.body)
   }
   const getTask = (req,res)=>{
       res.json({id:req.params.id})
